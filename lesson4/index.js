6b9aea31b499d73c61677f166d4b1db335c6f9de
mjmohona <mj.mohonajahan@gmail.com>
2025-05-22 17:01:08 +0600

    updated lesson 4

--- lesson4/index.js
+++ lesson4/index.js
@@ -1,7 +1,11 @@
 const http = require("http");
+const port = 3000;
+const hostname = '127.0.0.1' 
 
-http.createServer((req, res) => {
-   res.end("Hello iam your first server");
-}).listen(3000);
+const myServer = http.createServer((req, res) => {
+   res.end("<h1>Hello I'm your first server</h1>");
+})
 
-console.log(http)
+myServer.listen(port, ()=>{
+console.log(`Your server is running successfully at http://${hostname}:${port}`);
+});
